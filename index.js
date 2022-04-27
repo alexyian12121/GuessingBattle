a74be38c0978dab5574e4358966cf2bf144fa445
Kent Cortiguerra <74091292+alexyian12121@users.noreply.github.com>
2022-04-27 10:07:45 +0800

    Add files via upload

---
+++ index.js
@@ -0,0 +1,63 @@
+//location of the ship
+    var loc1=0;
+    var loc2=0;
+    var loc3=0;
+
+    //number of Guess
+    var guess=0;
+
+    // status of Battle
+     var ShipSunk=false;
+    
+     //Hits
+     var Hits=0;
+
+   //lives
+   var lives=10;
+
+   //how many times user try to guess
+   var tryuntildie=0;
+
+
+    function StartGame(){
+       
+
+
+        //start a game
+        while (ShipSunk==false){
+
+              //random location of the ship
+         loc1=String(Math.floor(Math.random()*25));
+         loc2=String(Math.floor(Math.random()*25));
+         loc3=String(Math.floor(Math.random()*25));
+         //since 24  po ang max number kaya po 25 ang limit ibig sabhin po mag eend sya bago 25 means hanggang 24 lan po 
+      
+            var guess=prompt("You only have " + lives +" to guess "+" Enter A number From 0-24 ");
+            
+            if (guess<0 || guess>24){
+                alert("You have entered an invalid number");
+            }else{
+                tryuntildie=tryuntildie+1;
+
+                if (guess==loc1 || guess==loc2 || guess==loc3){
+                    alert("You have hit the ship"); 
+                    Hits=Hits+1;
+                    if (Hits==3){
+                        alert("You WIN Congratulation");
+                        alert("ISANG MASIGABONG PAGBATI NG CONGRATULATIONS! You have only "+ lives +" lives left. And you Win");
+                        ShipSunk=true;
+
+                    }
+                }else{
+                    alert("You have missed the ship");
+                   lives--;
+                   
+                   if (lives==0){
+                       alert("You Lose")
+                      alert("You try " + tryuntildie + " times");
+                       ShipSunk=true
+                   }
+                }
+            }
+        }
+    }
